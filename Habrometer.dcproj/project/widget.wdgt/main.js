e6aeb97b1f472ee342ade66b83a713eb14d6f4f8
silvansky <v.silvansky@gmail.com>
2012-03-28 15:35:30 +0400

    initial commit - first working version

---
+++ Habrometer.dcproj/project/widget.wdgt/main.js
@@ -0,0 +1,154 @@
+var updateTimer;
+var updateInterval = (((60*1000) * 60) * 24);
+var preferenceKey = "habrometerUserName";
+var userImageUrl;
+
+// set true to enable debug output
+var debug = false;
+
+//-------------------------------------------------//
+function log(message)
+{
+    if (debug)
+    {
+        alert(message);
+    }
+}
+
+//-------------------------------------------------//
+function userName()
+{
+    return document.getElementById("nameEdit").value;
+}
+
+function setUserName(name)
+{
+    document.getElementById("nameEdit").value = name;
+}
+
+function userImageUrl()
+{
+    return "http://habrometr.ru/habrometr_425x120_" + userName() + ".png";
+}
+
+//-------------------------------------------------//
+function loadPrefs()
+{
+    var name = widget.preferenceForKey(widget.identifier + "-" + preferenceKey);
+    log(widget.identifier + "-" + preferenceKey);
+    log("name from preferences: " + name);
+    if (name != null)
+        setUserName(name);
+
+}
+
+function savePrefs()
+{
+    widget.setPreferenceForKey(userName(), widget.identifier + "-" + preferenceKey);
+}
+
+//-------------------------------------------------//
+function startTimer(msec)
+{
+    log("start timer");
+    updateTimer = setTimeout("updateStats()", msec);
+}
+
+function stopTimer()
+{
+    clearTimeout(updateTimer);
+}
+
+function updateStats()
+{
+    log("update");
+    var online = window.navigator.onLine;
+    if (online)
+    {
+        var url = userImageUrl();
+        log(url);
+        document.getElementById("habrometer").firstElementChild.src = url;
+    }
+    startTimer(updateInterval);
+}
+
+//-------------------------------------------------//
+function onLogoClicked()
+{
+    var websiteURL = "http://habrometr.ru/";
+    widget.openURL(websiteURL);
+}
+
+function load()
+{
+    log("load");
+    dashcode.setupParts();
+    loadPrefs();
+}
+
+function remove()
+{
+    stopTimer();
+    savePrefs();
+}
+
+function hide()
+{
+    stopTimer();
+    savePrefs();
+}
+
+function show()
+{
+    log("show");
+    loadPrefs();
+    startTimer(0);
+}
+
+function sync()
+{
+    loadPrefs();
+}
+
+function showBack(event)
+{
+    stopTimer();
+    var front = document.getElementById("front");
+    var back = document.getElementById("back");
+
+    if (window.widget) {
+        widget.prepareForTransition("ToBack");
+    }
+
+    front.style.display = "none";
+    back.style.display = "block";
+
+    if (window.widget) {
+        setTimeout('widget.performTransition();', 0);
+    }
+}
+
+function showFront(event)
+{
+    startTimer(0);
+    var front = document.getElementById("front");
+    var back = document.getElementById("back");
+
+    if (window.widget) {
+        widget.prepareForTransition("ToFront");
+    }
+
+    front.style.display="block";
+    back.style.display="none";
+
+    if (window.widget) {
+        setTimeout('widget.performTransition();', 0);
+    }
+}
+
+if (window.widget) {
+    widget.onremove = remove;
+    widget.onhide = hide;
+    widget.onshow = show;
+    widget.onsync = sync;
+}
